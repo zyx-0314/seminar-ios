a720cdd0a61b7a3d060827541b676b9ef421c49b
zyx-0314 <ramirezian037@gmail.com>
2024-08-16 09:58:07 +0800

    Initial Commit

---
+++ seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seminar
+//
+//  Created by Mac User on 8/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
